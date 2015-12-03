2b8327ef704c86301269d5093254adc5a0ac14db
njs55 <njs55@pitt.edu>
2015-12-02 22:08:53 -0500

    added two lines at end

--- hw8/prefix_sum.cu
+++ hw8/prefix_sum.cu
@@ -24,23 +24,24 @@ __global__ void scan_simple(float *g_odata, float *g_idata, int n) {
   g_odata[threadIdx.x] = 0.0;
            
     int thid = threadIdx.x;  
-    int1 pout = 0, pin = 1;  
+    int pout = 0, pin = 1;  
     // Load input into shared memory.  
      // This is exclusive scan, so shift right by one  
      // and set first element to 0  
     temp[pout*n + thid] = (thid > 0) ? g_idata[thid-1] : 0;  
     __syncthreads();  
-    for (int offset = 1; offset < n; offset *= 2)  
-    {  
+   for (int offset = 1; offset < n; offset *= 2) {  
       pout = 1 - pout; // swap double buffer indices  
       pin = 1 - pout;  
-      if (thid >= offset)  
-        temp[pout*n+thid] += temp[pin*n+thid - offset] + temp[pin*n+thid];  
-      else  
+      if (thid >= offset) {
+        temp[pout*n+thid] = temp[pin*n+thid - offset] + temp[pin*n+thid];  
+      }
+      else {  
         temp[pout*n+thid] = temp[pin*n+thid];  
+      }
       __syncthreads();  
-    }  
-    g_odata[thid] = temp[pout*n+thid1]; // write output  
+   }  
+   g_odata[thid] = temp[pout*n+thid]; // write output  
 
 }
 
@@ -51,7 +52,39 @@ __global__ void prescan(float *g_odata, float *g_idata, int n) {
   extern  __shared__  float temp[];  
 
   // STUDENT: YOUR CODE GOES HERE.
-
+  int thid = threadIdx.x;  
+  int offset = 1;  
+  
+  temp[2*thid] = g_idata[2*thid]; // load input into shared memory  
+  temp[2*thid+1] = g_idata[2*thid+1];
+  
+  // build sum in place up the tree 
+   for (int d = n>>1; d > 0; d >>= 1){   
+    __syncthreads();  
+      if (thid < d) {  
+           int ai = offset*(2*thid+1)-1;  
+           int bi = offset*(2*thid+2)-1; 
+           temp[bi] += temp[ai];  
+      }  
+      offset *= 2; 
+      if (thid == 0) { temp[n - 1] = 0; } // clear the last element 
+      // traverse down tree & build scan
+      for (int d = 1; d < n; d *= 2){  
+         offset >>= 1;  
+         __syncthreads();  
+         if (thid < d){  
+            int ai = offset*(2*thid+1)-1;  
+            int bi = offset*(2*thid+2)-1; 
+            float t = temp[ai];  
+            temp[ai] = temp[bi];  
+            temp[bi] += t;   
+         }  
+      }  
+      __syncthreads();
+      g_odata[2*thid] = temp[2*thid]; // write results to device memory
+		g_odata[2*thid+1] = temp[2*thid+1];
+   }
+         
 }
 
 /*
